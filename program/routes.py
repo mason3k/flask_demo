d6fb8eee39fb7ee177470c3055d6b659aeaa0e04
Sydney Jones <sydney.u.jones@gmail.com>
2023-10-01 14:12:51 -0500

    🚧 add basic templates

--- program/routes.py
+++ program/routes.py
@@ -1,10 +1,16 @@
 from program import app
+from flask import render_template
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello Pythonistas"
+    return render_template("index.html")
+
+
+@app.route("/100days")
+def _100days():
+    return render_template("100days.html")
 
 
 @app.route("/syd")
