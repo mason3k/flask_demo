6a118000b3de1c2c6ad564b8d5cded5d4e31a918
Sydney Jones <sydney.u.jones@gmail.com>
2023-10-06 15:56:11 -0500

    ✨ add min password length

--- program/auth.py
+++ program/auth.py
@@ -8,6 +8,10 @@ from program.db import get_db
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
+class InputError(Exception):
+    ...
+
+
 @bp.route("/register", methods=("GET", "POST"))
 def register():
     if request.method == "POST":
@@ -16,22 +20,25 @@ def register():
         db = get_db()
         error = None
 
-        if not username:
-            error = "Username is required."
-        elif not password:
-            error = "Password is required."
+        try:
+            if not username:
+                raise InputError("Username is required.")
+            elif not password:
+                raise InputError("Password is required.")
+            elif len(password) < 6:
+                raise InputError("Password must be at least 6 characters")
 
-        if error is None:
-            try:
-                db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
-                )
-                db.commit()
-            except db.IntegrityError:
-                error = f"User {username} is already registered."
-            else:
-                return redirect(url_for("auth.login"))
+            db.execute(
+                "INSERT INTO user (username, password) VALUES (?, ?)",
+                (username, generate_password_hash(password)),
+            )
+            db.commit()
+        except InputError as e:
+            error = str(e)
+        except db.IntegrityError:
+            error = f"User {username} is already registered."
+        else:
+            return redirect(url_for("auth.login"))
 
         flash(error)
 
@@ -44,15 +51,17 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         db = get_db()
-        error = None
         user = db.execute("SELECT * FROM user WHERE username = ?", (username,)).fetchone()
+        error = None
 
-        if user is None:
-            error = "Incorrect username."
-        elif not check_password_hash(user["password"], password):
-            error = "Incorrect password."
-
-        if error is None:
+        try:
+            if user is None:
+                raise InputError("Incorrect username.")
+            elif not check_password_hash(user["password"], password):
+                raise InputError("Incorrect password.")
+        except InputError as e:
+            error = str(e)
+        else:
             session.clear()
             session["user_id"] = user["id"]
             return redirect(url_for("index"))
