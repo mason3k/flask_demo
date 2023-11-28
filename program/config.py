31a03df691d72d81d32c03e6eec81d846aa82349
Sydney Jones <sydney.u.jones@gmail.com>
2023-11-28 13:56:17 -0600

    🔧 update default configuration

--- program/config.py
+++ program/config.py
@@ -2,5 +2,5 @@ from typing import Literal
 
 
 class Config:
-    ROWS_PER_PAGE: int = 10
-    DEFAULT_MODE: Literal["offline", "online"] = "online"
+    ROWS_PER_PAGE: int = 3
+    DEFAULT_MODE: Literal["offline", "online"] = "offline"
